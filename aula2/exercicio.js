d50a39c8bf7d3135085c89d49cdd48121c69a225
H3llyd4 <ralahet@gmail.com>
2025-12-06 20:25:11 -0300

    aula 2

---
+++ aula2/exercicio.js
@@ -0,0 +1,13 @@
+// nome, idade, número de telefone, cpf, endereço
+// string = armazenar texto
+// number = armazenar números
+// boolean = armazenar valores lógicos
+
+
+let pessoa = "Josicreide do Pythinho"
+let idade = 95
+let numetoTelefone = "(99) 99999-9999"
+let cpf = "999.999.999-00"
+let endereco = "Av. Paulista, bairro zona sul, número 0001"
+let recebePensao = true
+
---
+++ aula2/tiposDeVariaveis.js
@@ -0,0 +1,13 @@
+//pokemon
+//strings
+let nomePokemon = "Pikachu"
+let pokemonSexo = 'M'
+
+//number
+let nivelPokemon = 20
+let pontosDeVidaPokemon = 45
+
+//boolean
+let selecionavel = true
+
+// em outras linguagens exitem outros tipos de variáveis
