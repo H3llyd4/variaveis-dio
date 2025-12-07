8a3a1757eb2123b3834de0beb86a7ac4d7990553
H3llyd4 <ralahet@gmail.com>
2025-12-06 22:18:47 -0300

    Aula 3

---
+++ aula3/colecoes.js
@@ -0,0 +1,11 @@
+let pokemon = ["Pikachu" , "Charmander" , "Bulbasaur"] 
+let timePokemon = [
+    ["Pikachu", "M", "1"], 
+    ["Chamander", "F", "3"], 
+]
+//pokemon.pop() // remove o último elemento e retorna.
+//pokemon.shift() // remove o primeiro elemento e retorna.
+
+//console.log(timePokemon[1][0])
+
+console.log(" O pokemon " + timePokemon[1][0] + " é do sexo " + timePokemon[1][1] + " e está no nível " + timePokemon[1][2])
---
+++ aula3/matrizes.js
@@ -0,0 +1,7 @@
+let pokemon = ["Pikachu" , "Charmander" , "Bulbasaur"] 
+let timePokemon = [
+    ["Pikachu", "M", "1"], 
+    ["Chamander", "F", "3"], 
+]
+
+console.log(pokemon.length)
