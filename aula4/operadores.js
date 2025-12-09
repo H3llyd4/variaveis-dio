a5a91e5adbc79104c4002a2e32cfaec3105d6f4e
H3llyd4 <ralahet@gmail.com>
2025-12-08 23:04:18 -0300

    Create operadores

---
+++ aula4/operadores.js
@@ -0,0 +1,27 @@
+// Operadores aritiméticos 
+/*let soma = 5 + 4
+let subtracao = 5 - 4
+let multiplicacao = 5 * 4
+let divisao = 4 / 4
+
+
+console.log("soma = " + soma)
+console.log("subtração = " + subtracao)
+console.log("multiplicação = " + multiplicacao)
+console.log("divisão = " + divisao)*/
+
+let idade = 30
+
+console.log(idade)
+
+idade = 30 + 6
+
+console.log(idade)
+// incremento
+idade ++
+
+console.log(idade)
+// decremento
+idade --
+
+console.log(idade)
