9b0316d5da23ea4870298256631e76f6c03fe3a7
H3llyd4 <ralahet@gmail.com>
2025-12-10 23:48:13 -0300

    aula7

---
+++ aula8/fforwhile.js
@@ -0,0 +1,17 @@
+/* início do código
+// FOR
+
+for (let contador = 0;contador < 5; contador ++){
+    console.log(contador)
+    console.log("Aumentar o contador")
+}
+    fim do código 1*/
+
+let pontosDeVida = 0
+
+for (let i = 0; i < 10; i++) {
+    pontosDeVida += 1
+    console.log("Tomou poção mágica e recuperou: " + i + " pontos")
+}
+
+console.log(pontosDeVida + " pontos de vida Totais")
