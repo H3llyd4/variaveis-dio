b8c0edee4dedd03c3d77d5af2a77b0c5e4251013
H3llyd4 <ralahet@gmail.com>
2025-12-10 23:26:35 -0300

    Aula7

---
+++ aula7/switchcase.js
@@ -0,0 +1,26 @@
+
+
+// estrutura de decisão
+// switch/case/break/default
+
+let  fruta = "abacaxi"
+
+switch (fruta) {
+    case "abacaxi":
+        console.log("Suco de Abacaxi.")
+    break
+    case "laranja":
+        console.log("Suco de Laranja.")
+    break
+    case "banana":
+        console.log("Vitamina de Banana.")
+    break
+    case "morango":
+        console.log("Suco de Morango.")
+    case "maçã":
+        console.log("Suco de Maçã.")
+    break
+    default:
+        console.log("Não terá vitamina ou suco")
+    break
+}
